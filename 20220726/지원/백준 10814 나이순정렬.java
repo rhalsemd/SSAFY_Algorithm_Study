367cf59d53cb5ff9e04ca6226042bee3814ab015
jiwon199 <53902714+jiwon199@users.noreply.github.com>
2022-07-26 22:10:41 +0900

    Create 백준 10814 나이순정렬.java

---
+++ 20220726/지원/백준 10814 나이순정렬.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class temp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		StringBuilder[] sb = new StringBuilder[201];
+		String [] info=new String[201];
+		Set <Integer> ages=new HashSet<>();
+		int n=sc.nextInt();
+		for(int i=0;i<n;i++) {
+			int age=sc.nextInt();
+			String name=sc.next();
+			if(info[age]==null) {
+				info[age]=age+" "+name+"\n";
+			}
+			else {
+				info[age]+=age+" "+name+"\n";
+			}
+			ages.add(age);
+			 
+		}
+		
+		List<Integer> list=new ArrayList<>(ages);
+		Collections.sort(list); //오름차순 정렬
+		
+		for(int age: list) {
+			System.out.print(info[age]);
+		}
+		 
+	}
+
+}
