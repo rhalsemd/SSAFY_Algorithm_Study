922afde5670cfaf296ee95a3a19355cc73008748
bakuJahwi <58069290+Jahwi-0413@users.noreply.github.com>
2022-08-03 22:40:02 +0900

    Create Num_2164.java

---
+++ 8월/20220803/재희/Num_2164.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Num_2164 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		Deque<Integer>  deque = new LinkedList<>();
+		for(int i=n; i>=1; i--) {
+			deque.addLast(i);
+		}
+		
+		while(deque.size()>1) {
+			deque.pollLast();
+			int tmp = deque.pollLast();
+			deque.addFirst(tmp);
+		}
+		bw.write(deque.pollLast() + "");
+		bw.flush();
+		bw.close();
+	}
+}
