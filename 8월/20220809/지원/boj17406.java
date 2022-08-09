71b1535b4286ed3657c5d64b80f6a98a4a1b3935
jiwon199 <53902714+jiwon199@users.noreply.github.com>
2022-08-09 21:15:15 +0900

    Update boj17406.java

--- 8월/20220809/지원/boj17406.java
+++ 8월/20220809/지원/boj17406.java
@@ -21,7 +21,7 @@ public class Main {
 			 graph=new int[n][m];
 			 originGraph=new int[n][m];
 			 output=new int[k]; //순열 계산 위한 배열1
-			 visited=new boolean[k]; //순열 계 위한 배열2
+			 visited=new boolean[k]; //순열 계산 위한 배열2
 			 int answer=Integer.MAX_VALUE;
 			 perm(0,k);  
 			 for(int i=0;i<n;i++) {
